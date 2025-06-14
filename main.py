c5e2ef0132420abd8cfe3a4f90ea19a0f8a8e296
aaron-cherry <aaron.cherry10@gmail.com>
2025-06-13 22:30:38 -0700

    program takes command line args now

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
+import sys
 from stats import count_words
 from stats import count_chars
 from stats import print_report
 
 def get_book_text(file_path):
-   with open("./books/frankenstein.txt") as file:
+   with open(file_path) as file:
        print(f"Analyzing book found at {file_path}...")
        return file.read()
 
+def check_args(arg_list):
+    if len(arg_list) != 2:
+        print ("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
 def main():
     print("====== BOOKBOT =====")
-    book_text = get_book_text("./books/frankenstein.txt")
+    check_args(sys.argv)
+    book_text = get_book_text(sys.argv[1])
     word_count, words = count_words(book_text)
     print(word_count)
     print("----- Character Count ------")
